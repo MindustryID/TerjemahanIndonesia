a91e27ef871dc51c423a4228c3cdfec3168b3156
Kevin Vilyan <146146416+Kev-Vily@users.noreply.github.com>
2025-03-21 11:52:12 +0700

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const mindusindo = Vars.mods.locateMod("terjemahan-indonesia").meta;
+
+mindusindo.displayName = Core.bundle.get("mod.terjemahan-indonesia.name");
+mindusindo.author = Core.bundle.get("mod.terjemahan-indonesia.author");
+mindusindo.description = Core.bundle.get("mod.terjemahan-indonesia.description");
+mindusindo.subtitle = Core.bundle.get("mod.terjemahan-indonesia.subtitle");
