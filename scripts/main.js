fcdc00bd555ac5dfcc5273e181acee5c3e76b133
Kevin Vilyan <146146416+Kev-Vily@users.noreply.github.com>
2025-08-26 23:24:45 +0700

    Alternative Mod Info Translation

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,10 @@ mindusindo.displayName = Core.bundle.get("mod.terjemahan-indonesia.name");
 mindusindo.author = Core.bundle.get("mod.terjemahan-indonesia.author");
 mindusindo.description = Core.bundle.get("mod.terjemahan-indonesia.description");
 mindusindo.subtitle = Core.bundle.get("mod.terjemahan-indonesia.subtitle");
+
+const asthosus = Vars.mods.locateMod("asthosus").meta;
+
+asthosus.displayName = Core.bundle.get("mod.asthosus.name");
+asthosus.author = Core.bundle.get("mod.asthosus.author");
+asthosus.description = Core.bundle.get("mod.asthosus.description");
+asthosus.subtitle = Core.bundle.get("mod.asthosus.subtitle");
