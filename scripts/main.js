2dc6a6fe60f2a02fe868063b3d158f328673a76e
Kevin Vilyan <146146416+Kev-Vily@users.noreply.github.com>
2025-09-24 09:05:27 +0700

    add Exop-Axo-Xan Mod Info

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,24 @@ asthosus.displayName = Core.bundle.get("mod.asthosus.name");
 asthosus.author = Core.bundle.get("mod.asthosus.author");
 asthosus.description = Core.bundle.get("mod.asthosus.description");
 asthosus.subtitle = Core.bundle.get("mod.asthosus.subtitle");
+
+const exoprosopa = Vars.mods.locateMod("exoprosopa").meta;
+
+exoprosopa.displayName = Core.bundle.get("mod.exoprosopa.name");
+exoprosopa.author = Core.bundle.get("mod.exoprosopa.author");
+exoprosopa.description = Core.bundle.get("mod.exoprosopa.description");
+exoprosopa.subtitle = Core.bundle.get("mod.exoprosopa.subtitle");
+
+const axoxium = Vars.mods.locateMod("axoxium").meta;
+
+axoxium.displayName = Core.bundle.get("mod.axoxium.name");
+axoxium.author = Core.bundle.get("mod.axoxium.author");
+axoxium.description = Core.bundle.get("mod.axoxium.description");
+axoxium.subtitle = Core.bundle.get("mod.axoxium.subtitle");
+
+const xanthia = Vars.mods.locateMod("xanthia-3").meta;
+
+xanthia.displayName = Core.bundle.get("mod.xanthia.name");
+xanthia.author = Core.bundle.get("mod.xanthia.author");
+xanthia.description = Core.bundle.get("mod.xanthia.description");
+xanthia.subtitle = Core.bundle.get("mod.xanthia.subtitle");
